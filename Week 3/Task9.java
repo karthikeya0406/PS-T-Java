964de3d9bdc1d62c91c3d8e7c59802b22711b1ef
karthikeya0406 <vtu25740@veltech.edu.in>
2026-02-05 14:25:15 +0530

    Update Task9.java

--- Week 3/Task9.java
+++ Week 3/Task9.java
@@ -6,16 +6,16 @@ public class Task9 {
         String[] words = input1.split(" ");
         int pin = 0;
 
-        // Sum of word lengths
+  
         for (String word : words) {
             pin += word.length();
         }
 
-        // If single digit, return
+     
         if (String.valueOf(pin).length() == 1)
             return pin;
 
-        // Otherwise sum digits
+     
         int pin2 = 0;
         String pinStr = String.valueOf(pin);
 
